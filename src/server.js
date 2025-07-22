016ba47d30b3e0075aac574ab001e26d6283996f
Yestay <yestayyessenbay@gmail.com>
2025-07-22 21:26:14 +0900

    feat(server): Implement Websocket on top of http

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -9,7 +11,12 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('home', { pageTitle: 'Home' });
 });
+app.get('/*', (req, res) => res.redirect('/'));
 
 console.log('Hello, World!');
 
-app.listen(3000);
+const server = http.createServer(app);
+
+const wss = WebSocket.Server({ server });
+
+server.listen(3000);
