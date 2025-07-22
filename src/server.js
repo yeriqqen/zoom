86a234c2fb8457360fb092cb51d9db2c5d310057
Yestay <yestayyessenbay@gmail.com>
2025-07-22 15:26:37 +0900

    feat(server): Add server files

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home', { pageTitle: 'Home' });
+});
+
+console.log('Hello, World!');
+
+app.listen(3000);
